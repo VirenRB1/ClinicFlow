a3a6720e0fb9ab7532bc4d37e985875f0150b58f
Viren <bhanderiviren@gmail.com>
2026-02-03 17:42:14 -0600

    1. Adding the stub database Interface

---
+++ app/src/main/java/com/example/clinicflow/persistence/UserRepository.java
@@ -0,0 +1,25 @@
+package com.example.clinicflow.persistence;
+
+import com.example.clinicflow.model.Doctor;
+import com.example.clinicflow.model.Patient;
+import com.example.clinicflow.model.OfficeStaff;
+
+import java.util.List;
+
+public interface UserRepository {
+    List<Doctor> getAllDoctors();
+    List<Patient> getAllPatients();
+    List<OfficeStaff> getAllOfficeStaff();
+
+    void addDoctor(Doctor doctor);
+    void addPatient(Patient patient);
+    void addOfficeStaff(OfficeStaff officeStaff);
+
+    Doctor getDoctorById(int id);
+    Patient getPatientById(int id);
+    OfficeStaff getOfficeStaffById(int id);
+
+    boolean deleteDoctor(int id);
+    boolean deletePatient(int id);
+    boolean deleteOfficeStaff(int id);
+}
