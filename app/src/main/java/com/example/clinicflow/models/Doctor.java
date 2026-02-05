7d9a9678f62b8a107f9e9bece174dd86c52c95cb
Viren <bhanderiviren@gmail.com>
2026-02-04 22:06:13 -0600

    Fixing up the objects data in model to add firstName and lastName for Users, Staff, Doctors, Patients

--- app/src/main/java/com/example/clinicflow/models/Doctor.java
+++ app/src/main/java/com/example/clinicflow/models/Doctor.java
@@ -1,12 +1,20 @@
 package com.example.clinicflow.models;
 public class Doctor extends Users {
-    private String specialization;  //also expereience
+    private String specialization;  //also experience
     private String licenseNumber;
 
     public Doctor(String email, String password, String gender,
-                  String age, String specialization, String licenseNumber) {
+                  int age, String specialization, String licenseNumber) {
         super(email, password, gender, age);
         this.specialization = specialization;
         this.licenseNumber = licenseNumber;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
     }
 }
