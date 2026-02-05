a52e472a175947b9b79bce56c1a613c4a9479ab2
gurwinderkhandal41 <gurwinderkhandal@gmail.com>
2026-02-05 11:33:41 -0600

    Fixed some navigation issues when logging in. Users are now taken to their respective landing screen. Logout buttons work now.

--- app/src/main/java/com/example/clinicflow/presentation/DoctorScreen.java
+++ app/src/main/java/com/example/clinicflow/presentation/DoctorScreen.java
@@ -1,6 +1,10 @@
 package com.example.clinicflow.presentation;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +12,67 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.clinicflow.MainActivity;
 import com.example.clinicflow.R;
 
 public class DoctorScreen extends AppCompatActivity {
+
+    Button logout;
+
+    ImageButton profile;
+
+    Button mySchd;
+
+    Button setAvail;
+
+    Button patientRecs;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.doctor_landing);
+
+        logout = findViewById(R.id.logoutButton);
+        profile = findViewById(R.id.profileButton);
+        mySchd = findViewById(R.id.myScheduleButton);
+        setAvail = findViewById(R.id.setAvailabilityButton);
+        patientRecs = findViewById(R.id.patientRecordsButton);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DoctorScreen.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mySchd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        setAvail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        patientRecs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
--- app/src/main/java/com/example/clinicflow/presentation/PatientScreen.java
+++ app/src/main/java/com/example/clinicflow/presentation/PatientScreen.java
@@ -1,6 +1,10 @@
 package com.example.clinicflow.presentation;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +12,57 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.clinicflow.MainActivity;
 import com.example.clinicflow.R;
 
 public class PatientScreen extends AppCompatActivity {
+
+    Button logout;
+
+    Button myApts;
+
+    Button bookApt;
+
+    ImageButton profile;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.patient_landing);
+
+        logout = findViewById(R.id.logoutButton);
+        myApts = findViewById(R.id.myAppointmentsButton);
+        bookApt = findViewById(R.id.bookAppointmentButton);
+        profile = findViewById(R.id.profileButton);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PatientScreen.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        myApts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        bookApt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
