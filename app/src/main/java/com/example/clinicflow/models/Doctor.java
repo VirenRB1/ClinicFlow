1b93f8bcf988f5458ab3de2f866e477604216eeb
Viren <bhanderiviren@gmail.com>
2026-02-04 22:25:31 -0600

    Adding firstName, lastName for Doctor object as I forgot in last commit

--- app/src/main/java/com/example/clinicflow/models/Doctor.java
+++ app/src/main/java/com/example/clinicflow/models/Doctor.java
@@ -3,9 +3,9 @@ public class Doctor extends Users {
     private String specialization;  //also experience
     private String licenseNumber;
 
-    public Doctor(String email, String password, String gender,
+    public Doctor(String firstName, string lastName, String email, String password, String gender,
                   int age, String specialization, String licenseNumber) {
-        super(email, password, gender, age);
+        super(firstName, lastName, email, password, gender, age);
         this.specialization = specialization;
         this.licenseNumber = licenseNumber;
     }
