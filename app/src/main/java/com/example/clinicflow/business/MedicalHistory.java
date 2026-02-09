336cef177e927e44486026925135263fa8ec20de
Viren <bhanderiviren@gmail.com>
2026-02-08 23:01:51 -0600

    Added MedicalHistory class that will deal will all backend processing for the Medical Record Feature. It uses a shared stub database with AuthService as of now

--- app/src/main/java/com/example/clinicflow/business/MedicalHistory.java
+++ app/src/main/java/com/example/clinicflow/business/MedicalHistory.java
@@ -1,6 +1,9 @@
 package com.example.clinicflow.business;
 
+import com.example.clinicflow.models.MedicalRecord;
 import com.example.clinicflow.persistence.fake.FakeUserRepository;
+
+import java.util.List;
 
 public class MedicalHistory {
     private final FakeUserRepository DATABASE;
@@ -8,4 +11,15 @@ public class MedicalHistory {
     public MedicalHistory(FakeUserRepository userRepository) {
         this.DATABASE = userRepository;
     }
+
+    // Use this method to get a list of MedicalRecord in the frontend
+    //Please note the list could also be empty so handle that case
+    // Alice Brown has 2 records, Charlie Wilson has 1 record and Bob Davis has no records
+    //The function will sort the records by date in descending order so the latest record will be first
+    public List<MedicalRecord> getSortedMedicalHistoryForPatient(String patientName) {
+        List<MedicalRecord> medicalRecords = DATABASE.getMedicalRecords(patientName);
+        medicalRecords.sort((record1, record2) -> record2.getDate().compareTo(record1.getDate()));
+        return medicalRecords;
+    }
+
 }
