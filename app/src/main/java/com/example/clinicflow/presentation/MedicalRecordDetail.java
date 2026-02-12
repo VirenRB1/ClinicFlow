b509737fe445b141d8e4767ee6b0202b720fb0f2
gurwinderkhandal41 <gurwinderkhandal@gmail.com>
2026-02-11 21:41:45 -0600

    Added Back Button to medical_record_detail.xml Added onClickListener for back button for that page. Attached email with intent.

--- app/src/main/java/com/example/clinicflow/presentation/MedicalRecordDetail.java
+++ app/src/main/java/com/example/clinicflow/presentation/MedicalRecordDetail.java
@@ -1,6 +1,9 @@
 package com.example.clinicflow.presentation;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,13 +19,25 @@ import java.util.Locale;
 public class MedicalRecordDetail extends AppCompatActivity {
 
     public static final String EXTRA_RECORD = "Record";
+    public static final String EXTRA_USER_EMAIL = "user_email";
     private final SimpleDateFormat dateFmt = new SimpleDateFormat("MMM d, yyyy", Locale.getDefault());
 
+    Button backBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.medical_record_detail);
+
+        backBtn = findViewById(R.id.backButton);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MedicalRecordDetail.this, MyRecords.class);
+                intent.putExtra(EXTRA_USER_EMAIL, getIntent().getStringExtra(EXTRA_USER_EMAIL));
+                startActivity(intent);
+            }
+        });
 
         MedicalRecord record = (MedicalRecord) getIntent().getSerializableExtra(EXTRA_RECORD);
         if (record == null) {
