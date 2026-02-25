41ab32b1b7df70b0f2fab037d0de0c3e7c3bf2da
gurwinderkhandal41 <gurwinderkhandal@gmail.com>
2026-02-25 11:07:31 -0600

    Refactored the SignupScreen.java

--- app/src/main/java/com/example/clinicflow/application/ClinicFlowApp.java
+++ app/src/main/java/com/example/clinicflow/application/ClinicFlowApp.java
@@ -1,6 +1,7 @@
 package com.example.clinicflow.application;
 
 import com.example.clinicflow.business.AuthService;
+import com.example.clinicflow.business.ObjectCreation;
 import com.example.clinicflow.persistence.UserRepository;
 import com.example.clinicflow.persistence.fake.FakeUserRepository;
 //Startup app
@@ -9,11 +10,19 @@ public class ClinicFlowApp extends android.app.Application {
 
     private AuthService authService;
 
+    private ObjectCreation objectCreation;
+
+
     @Override
     public void onCreate() {
         super.onCreate();
         userRepository = new FakeUserRepository();
         authService = new AuthService(userRepository);
+        objectCreation = new ObjectCreation(userRepository);
+    }
+
+    public ObjectCreation getObjectCreation() {
+        return objectCreation;
     }
 
     public AuthService getAuthService() {
