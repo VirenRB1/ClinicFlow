745c9470ae62b2c8362cb39622c6e7232335d8d8
Viren <bhanderiviren@gmail.com>
2026-03-07 14:52:06 -0600

    Changing database version to use a new database instance and reset all users

--- app/src/main/java/com/example/clinicflow/persistence/real/AppDbHelper.java
+++ app/src/main/java/com/example/clinicflow/persistence/real/AppDbHelper.java
@@ -8,7 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class AppDbHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "clinic_flow.db";
-    public static final int DATABASE_VERSION = 4;
+    public static final int DATABASE_VERSION = 5;
 
     public AppDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
