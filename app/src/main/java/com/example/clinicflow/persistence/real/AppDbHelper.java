5c31199ea274a7a538447d54fcc83cd55ffb05c9
Viren <bhanderiviren@gmail.com>
2026-03-13 03:06:50 -0500

    Updating a new database version

--- app/src/main/java/com/example/clinicflow/persistence/real/AppDbHelper.java
+++ app/src/main/java/com/example/clinicflow/persistence/real/AppDbHelper.java
@@ -12,7 +12,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class AppDbHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "clinic_flow.db";
-    public static final int DATABASE_VERSION = 8;
+    public static final int DATABASE_VERSION = 9;
 
     /**
      * Constructs a new database helper.
